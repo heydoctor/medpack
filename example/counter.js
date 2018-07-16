52113828cdff617b2bee5efb8d22b9a339077c5a
Kyle Alwyn <kjalwyn@gmail.com>
2018-07-15 20:53:44 -0700

    chore(release): 0.0.4

---
+++ example/counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>1</span>
+      </div>
+    );
+  }
+}
--- example/index.js
+++ example/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Counter from './counter';
 import './index.scss';
 import styles from './index.module.scss';
+import logo from './logo.svg';
 
-const App = () => <h1 className={styles.title}>Hello Quantumpack!</h1>;
+const App = () => (
+  <div>
+    <img src={logo} alt="" width="100" />
+    <h1 className={styles.title}>Hello Quantumpack!</h1>
+    <Counter />
+  </div>
+);
 
 const rootEl = document.getElementById('root');
 
