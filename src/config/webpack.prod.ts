4b45d9147328650738b1781ac973615503ecfc3c
Kyle Alwyn <kjalwyn@gmail.com>
2018-09-14 11:01:04 -0700

    chore: Upgrade packages

--- src/config/webpack.prod.ts
+++ src/config/webpack.prod.ts
@@ -11,6 +11,7 @@ export default ({ paths, sourceMaps }: IWebpackConfig): Configuration => {
 
   return {
     mode: 'production',
+    bail: true,
     entry: [`${__dirname}/polyfills`, paths.appIndexJs],
     output: {
       // The build folder.
@@ -28,16 +29,7 @@ export default ({ paths, sourceMaps }: IWebpackConfig): Configuration => {
       minimizer: [
         new UglifyJsPlugin({
           uglifyOptions: {
-            parse: {
-              // we want uglify-js to parse ecma 8 code. However, we don't want it
-              // to apply any minfication steps that turns valid ecma 5 code
-              // into invalid ecma 5 code. This is why the 'compress' and 'output'
-              // sections only apply transformations that are ecma 5 safe
-              // https://github.com/facebook/create-react-app/pull/4234
-              ecma: 8,
-            },
             compress: {
-              ecma: 5,
               warnings: false,
               // Disabled because of an issue with Uglify breaking seemingly valid code:
               // https://github.com/facebook/create-react-app/issues/2376
@@ -45,11 +37,7 @@ export default ({ paths, sourceMaps }: IWebpackConfig): Configuration => {
               // https://github.com/mishoo/UglifyJS2/issues/2011
               comparisons: false,
             },
-            mangle: {
-              safari10: true,
-            },
             output: {
-              ecma: 5,
               comments: false,
               // Turned on because emoji and regex is not minified properly using default
               // https://github.com/facebook/create-react-app/issues/2488
