9b48d809ee6f2ac6827db108453a17e21092c77f
Kyle Alwyn <kjalwyn@gmail.com>
2019-02-22 11:43:25 -0800

    Add more functionality to example

--- example/index.js
+++ example/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Counter from './counter';
+import Counter from './components/Counter';
 import './index.scss';
 import styles from './index.module.scss';
 import logo from './logo.svg';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={logo} alt="" width="100" />
-        <h1 className={styles.pageTitle}>Hello medpack!</h1>
-        <Counter />
-        <span>{process.env.FROM_ENV}</span>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <img src={logo} alt="" width="100" />
+      <h1 className={styles.pageTitle}>Hello medpack!</h1>
+      <Counter />
+      <br />
+      <span>We can also use the environment variables you make available in medpack.js:</span>
+      <pre>{JSON.stringify(process.env, null, 2)}</pre>
+    </>
+  )
 }
 
 const rootEl = document.getElementById('root');
