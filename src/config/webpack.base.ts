0dd0c3eda4e28e5dca040b1be73270756e6ba42e
Alex Tran <atran@goodrx.com>
2020-01-07 11:00:34 -0800

    add jsx extension to resolve

--- src/config/webpack.base.ts
+++ src/config/webpack.base.ts
@@ -76,7 +76,7 @@ export default ({ mode, paths, env, sourceMaps }: IWebpackConfig): Configuration
 
   return {
     resolve: {
-      extensions: ['.ts', '.tsx', '.js']
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     output: {
       // Point sourcemap entries to original disk location (format as URL on Windows)
